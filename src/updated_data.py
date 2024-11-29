72fba894d859b6b7daf77a1cbbc8ec7036448720
fickya1987 <89722540+fickya1987@users.noreply.github.com>
2024-11-29 09:24:49 +0700

    Update updated_data.py

--- src/updated_data.py
+++ src/updated_data.py
@@ -13,6 +13,11 @@ def load_transform(file) -> DataFrame:
     df['ToBePromoted'] = ((df['YearsSinceLastPromotion'] >= 5) & 
                           (df['PerformanceRating'] > 2)).astype(int)
 
+    # Feature Engineering for Retrenchment Analysis
+    df["ToBeRetrenched"] = ((df["YearsInCurrentRole"] >= 10) & (df["PerformanceRating"] < 3)) | \
+                           ((3 <= df["YearsInCurrentRole"]) & (df["YearsInCurrentRole"] < 10) & (df["PerformanceRating"] == 1))
+    df["ToBeRetrenched"] = df["ToBeRetrenched"].map({True: "Yes", False: "No"})
+
     return df
 
 
@@ -39,56 +44,13 @@ def get_filter_options(df, empty_filters=False):
     return filter_opt
 
 
-def get_dept_stats_df(df: DataFrame):
-    """Calculate various stats for each department and returns results in a DataFrame"""
-    df_dept = (
-        df.groupby("Department")[
-            [
-                "MonthlyIncome",
-                "PercentSalaryHike",
-                "TotalWorkingYears",
-                "YearsAtCompany",
-                "TrainingTimesLastYear",
-            ]
-        ]
-        .mean()
-        .round(2)
-    )
-    df_ot = (
-        df.query("OverTime == 'Yes'")
-        .groupby("Department")["OverTime"]
-        .count()
-        .to_frame()
-    )
-    df_dept_stat = df_dept.join(df_ot, how="inner").reset_index()
-    return df_dept_stat
-
-
-def get_gender_count(df: DataFrame):
-    """Calculates employee total count and for each gender,
+def get_retrench_count(df: DataFrame):
+    """Calculates number of employees due for retrenchment,
     returns count and percentage as result"""
-    df_gender = df.groupby("Gender").size()
-    male_emp_cnt = df_gender.get("Male", 0)
-    female_emp_cnt = df_gender.get("Female", 0)
-    tot_emp_cnt = male_emp_cnt + female_emp_cnt
-    male_pct = round((male_emp_cnt / tot_emp_cnt) * 100, 2)
-    female_pct = round((female_emp_cnt / tot_emp_cnt) * 100, 2)
-    return tot_emp_cnt, male_emp_cnt, female_emp_cnt, male_pct, female_pct
-
-
-def get_promo_count(df: DataFrame):
-    """Calculates number of employees due for promotion,
-    returns count and percentage as result"""
-    df_promo = df.groupby("ToBePromoted").size()
-    promo_cnt = df_promo.get(1, 0)  # 1 represents "Yes" in binary
-    not_promo_cnt = df_promo.get(0, 0)
-    tot_emp_cnt, _, _, _, _ = get_gender_count(df)
-    promo_pct = round((promo_cnt / tot_emp_cnt) * 100, 2)
-    not_promo_pct = round((not_promo_cnt / tot_emp_cnt) * 100, 2)
-    return promo_cnt, not_promo_cnt, promo_pct, not_promo_pct
-
-
-def df_to_csv(file: str) -> bytes:
-    """Read file again and return as as a csv"""
-    df = pd.read_csv(file)
-    return df.to_csv(index=False).encode("utf-8")
+    df_retrench = df.groupby("ToBeRetrenched").size()
+    retrench_cnt = df_retrench.get("Yes", 0)
+    not_retrench_cnt = df_retrench.get("No", 0)
+    tot_emp_cnt = len(df)
+    retrench_pct = round((retrench_cnt / tot_emp_cnt) * 100, 2)
+    not_retrench_pct = round((not_retrench_cnt / tot_emp_cnt) * 100, 2)
+    return retrench_cnt, not_retrench_cnt, retrench_pct, not_retrench_pct
